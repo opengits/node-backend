0f94c95dea0cae2d563fe9e003b282ed5f3c9297
opengits <kamalsharma.git@gmail.com>
2024-08-15 11:13:27 +0100

    Final Commit

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // Initialize the data source before creating the Nest application
-  AppModule;
+  
   
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', // Allow requests from all origin for getting list in html page
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
